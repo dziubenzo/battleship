4e34c0be419aa833a1c2ed1eb92999c35b127952
dziubenzo <dziubenzo@gmail.com>
2024-01-12 11:01:46 +0100

    Write first Gameboard class tests

---
+++ src/js/__tests__/gameboard.test.js
@@ -0,0 +1,37 @@
+/* eslint no-undef: 0 */
+
+import { Gameboard } from '../gameboard';
+
+// Gameboard class tests
+describe('Gameboard', () => {
+  const gameboard = new Gameboard();
+
+  test('creates 10x10 board', () => {
+    expect(gameboard.board.length).toHaveLength(100);
+  });
+
+  test.each([
+    [0, 0, null],
+    [0, 9, null],
+    [9, 0, null],
+    [9, 9, null],
+    [4, 5, null],
+  ])('board square [%i][%i] is %s', (row, column, expected) => {
+    expect(gameboard.board[row][column]).toBe(expected);
+  });
+
+  test.each([
+    [-1, 5],
+    [0, 10],
+    [5, -5],
+    [12, 3],
+    [-1, -5],
+  ])(
+    'throws error when accessing non-existing square [%i][%i]',
+    (row, column) => {
+      expect(() => {
+        gameboard.board[row][column];
+      }).toThrow('Board square does not exist');
+    },
+  );
+});
