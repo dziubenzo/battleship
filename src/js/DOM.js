90977ad61dc6f227394fbdd695b60f0eee821572
dziubenzo <dziubenzo@gmail.com>
2024-01-16 18:09:58 +0100

    Add descriptions to board headers

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,7 +1,7 @@
 import gitHubLogoWhite from '../assets/github-logo-white.svg';
 import gitHubLogoBlack from '../assets/github-logo-black.svg';
 
-const SQUARES = 121;
+const NO_OF_SQUARES = 121;
 
 // Load images
 export function loadImages() {
@@ -9,7 +9,7 @@ export function loadImages() {
   gitHubLogo.src = gitHubLogoWhite;
 }
 
-// Generate squares
+// Generate board squares
 export function generateSquares() {
   const playerBoard = document.querySelector('div[class="player-board"]');
   const enemyBoard = document.querySelector('div[class="enemy-board"]');
@@ -21,18 +21,18 @@ export function generateSquares() {
   for (const board of boards) {
     let row = 0;
     let column = 0;
-    for (let i = 0; i < SQUARES; i++) {
+    for (let i = 0; i < NO_OF_SQUARES; i++) {
       const square = document.createElement('div');
       square.classList.add('square');
-      square.dataset.id = i + 1;
+      square.dataset.id = i;
+      board.append(square);
+      // Do not add column or row data attributes to headers
       if (headers.includes(i)) {
         square.classList.add('header');
-        board.append(square);
         continue;
       }
       square.dataset.row = row;
       square.dataset.column = column;
-      board.append(square);
       if (column === 9) {
         column = 0;
         row++;
@@ -41,4 +41,36 @@ export function generateSquares() {
       column++;
     }
   }
+  addHeaders();
+}
+
+// Add header descriptions to board headers
+function addHeaders() {
+  const headerDescriptions = {
+    0: '',
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: '10',
+    11: 'A',
+    22: 'B',
+    33: 'C',
+    44: 'D',
+    55: 'E',
+    66: 'F',
+    77: 'G',
+    88: 'H',
+    99: 'I',
+    110: 'J',
+  };
+  const headers = document.querySelectorAll('.square.header');
+  headers.forEach((header) => {
+    header.textContent = headerDescriptions[header.dataset.id];
+  });
 }
